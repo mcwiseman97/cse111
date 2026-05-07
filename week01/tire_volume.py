796934b4676076eedda01fab15540600aeedbf8c
Michael Wiseman <mcwiseman97@gmail.com>
2026-05-07 14:46:04 -0600

    fix: correct import statement and variable name for tire volume calculation

--- week01/tire_volume.py
+++ week01/tire_volume.py
@@ -1,9 +1,26 @@
 import math as m
+import datetime from datetime
+
+dt = datetime
 
 tireWidth = int(input("Enter the width of the tire in mm (ex 205): "))
 aspectRatio = int(input("nter the aspect ratio of the tire (ex 60): "))
 tireDiameter = int(input("Enter the diameter of the wheel in inches (ex 15): "))
 
-tireVolume = m.pi * (tireWidth**2) * aspectRatio * (tireWidth * aspectRatio + 2540 * tireDiameter) / 10_000_000_000
+tireVolumeL = m.pi * (tireWidth**2) * aspectRatio * (tireWidth * aspectRatio + 2540 * tireDiameter) / 10_000_000_000
 
-print(f"The approximate volume is {tireVolume:.2f} liters")
+print(f"The approximate volume is {tireVolumeL:.2f} liters")
+
+
+
+
+
+
+#Get the current date from the computer’s operating system.
+#Open a text file named volumes.txt for appending.
+#Append to the end of the volumes.txt file one line of text that contains the following five values:
+# 1. current date (Do NOT include time)
+# 2. width of the tire
+# 3. aspect ratio of the tire
+# 4. diameter of the wheel
+# 5. volume of the tire (rounded to two decimal places)
