e3ceb2c010e9a8c6fc566ccfde87d8077c2d2ec5
Michael Wiseman <mcwiseman97@gmail.com>
2026-05-09 13:00:44 -0500

    refactor: clean up code structure and improve readability

--- week01/tire_volume.py
+++ week01/tire_volume.py
@@ -1,27 +1,50 @@
-import math as m
+# tire_volume.py
+# CSE 111 - Week 01 Project
+
+# EXCEEDING REQUIREMENTS:
+# This version asks the user if they want to get tire newletters.
+# If they answer yes, the program stores their email in the volumes.txt file along with the tire information.
+
+import math
 from datetime import datetime as dt
 
-todaysDate = dt.now()
-# {todaysDate:%Y-%m-%d}
+# user answers for tire info
+width = int(input("Enter the width of the tire in mm (ex 205): "))
+aspect_ratio = int(input("Enter the aspect ratio of the tire (ex 60): "))
+diameter = int(input("Enter the diameter of the wheel in inches (ex 15): "))
 
-#tireWidth = int(input("Enter the width of the tire in mm (ex 205): "))
-#aspectRatio = int(input("nter the aspect ratio of the tire (ex 60): "))
-#tireDiameter = int(input("Enter the diameter of the wheel in inches (ex 15): "))
+volume = (math.pi * width**2 * aspect_ratio * (width * aspect_ratio + 2540 * diameter) / 10000000000)
 
-#tireVolumeL = m.pi * (tireWidth**2) * aspectRatio * (tireWidth * aspectRatio + 2540 * tireDiameter) / 10_000_000_000
+print(f"The approximate volume is {volume:.2f} liters")
 
-#print(f"The approximate volume is {tireVolumeL:.2f} liters")
+current_date = dt.now()
 
-print(f"{dt.now():%Y-%m-%d}")
+newsletter_tires = input("Do you want to subscribe for a newsletter? (yes/no): ").lower()
+user_email = ""
 
+# If the user says yes, ask for their email
+if newsletter_tires == "yes":
+    user_email = input("Please enter your email: ")
 
+with open("volumes.txt", "at") as file:
 
+    if user_email != "":
+        print(
+            f"{current_date:%Y-%m-%d}, "
+            f"{width}, "
+            f"{aspect_ratio}, "
+            f"{diameter}, "
+            f"{volume:.2f}, "
+            f"{user_email}",
+            file=file
+        )
 
-#Get the current date from the computer’s operating system.
-#Open a text file named volumes.txt for appending.
-#Append to the end of the volumes.txt file one line of text that contains the following five values:
-# 1. current date (Do NOT include time)
-# 2. width of the tire
-# 3. aspect ratio of the tire
-# 4. diameter of the wheel
-# 5. volume of the tire (rounded to two decimal places)
+    else:
+        print(
+            f"{current_date:%Y-%m-%d}, "
+            f"{width}, "
+            f"{aspect_ratio}, "
+            f"{diameter}, "
+            f"{volume:.2f}",
+            file=file
+        )
