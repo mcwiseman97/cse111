371111cd8fb1dead8787b62585d3f23e55c6335d
Michael Wiseman <mcwiseman97@gmail.com>
2026-05-13 21:48:51 -0500

    Refactor code structure for improved readability and maintainability

---
+++ Week02/password.py
@@ -0,0 +1,8 @@
+"""
+1. Create a folder for this week's project, name it whatever you want.
+2. Open the folder you just created in VSCode.
+3. Create a file named passwords.py.
+4. Add the Constants that define the character lists given from Sven to the top of your code.
+5. Create a function definition for each of the functions defined by Sven. Since you won't have any code in your functions yet just add the word pass as the body of your function.
+6. Download the following files and save them in your project directory:
+"""
