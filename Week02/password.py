e2bfc2424d794adc6fd66a3c8826e37f0918cbd6
Michael Wiseman <mcwiseman97@gmail.com>
2026-05-13 21:51:03 -0500

    feat: add initial structure for password generation project

--- Week02/password.py
+++ Week02/password.py
@@ -5,4 +5,16 @@
 4. Add the Constants that define the character lists given from Sven to the top of your code.
 5. Create a function definition for each of the functions defined by Sven. Since you won't have any code in your functions yet just add the word pass as the body of your function.
 6. Download the following files and save them in your project directory:
+
+USE: open(filename, "r",encoding="utf-8")
 """
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
